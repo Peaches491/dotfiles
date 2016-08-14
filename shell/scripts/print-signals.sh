5965d6b90b39645e67a6d09c5f5da0bb329cc2a4
Daniel Miller <daniel@zoox.com>
2016-08-13 22:18:30 -0700

    added print-signals.sh

---
+++ shell/scripts/print-signals.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+trap_with_arg() {
+    func="$1" ; shift
+    for sig ; do
+        trap "$func $sig" "$sig"
+    done
+}
+
+func_trap() {
+    echo Trapped: $1
+}
+
+trap_with_arg func_trap INT TERM EXIT
+
+read # Wait so the script doesn't exit.
